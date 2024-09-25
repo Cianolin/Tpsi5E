e60ee289d0f0b6595dda2a0ebc13863d28de26e0
Luciano Lin <luciano.lin@iisviolamarchesini.edu.it>
2024-09-25 19:06:51 +0200

    continuo esercizio

--- es1°.c
+++ es1°.c
@@ -60,16 +60,21 @@ void evenN(int Vettore[])
         {
             printf("%d,", Vettore[i]);
         }
-    } }
+    }
+}
 // 5. Visualizza dispari
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 // Scambia arr[j] e arr[j+1]
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
@@ -109,7 +114,7 @@ void Delete(int vettore[], int num)
     nVettore--;
 }
 // 8. N alterni
-int* Alterni(int vettore[])
+int *Alterni(int vettore[])
 {
     int vettore2[nVettore];
     for (int i = 0; i < nVettore; i++)
@@ -123,10 +128,10 @@ int* Alterni(int vettore[])
             vettore2[i] = vettore[i - 1];
         }
     }
-    int *x=vettore2;
+    int *x = vettore2;
     return x;
 }
-//bubble sort
+// bubble sort
 void stampaArray(int *vettore)
 {
     for (int i = 0; i < nVettore; i++)
