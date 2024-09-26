8fed7b7c7436ea8b787329af1b75033e1bf9b6fd
Cianolin <145426146+Cianolin@users.noreply.github.com>
2024-09-26 08:55:31 +0200

    es2°

---
+++ es2°.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void controllo(){
+    int count=0;
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if((argv[1]>=97 && argv[1]<=122) || (argv[1]>= 65&&argv[1]<90)){
+            count++;
+        }
+    }
+    
+}
+void trova(char letter){
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if(argv[1]==letter){
+            
+        }
+    }
+    
+}
+int main(int arg, char *agv[]){
+    string 
+}
