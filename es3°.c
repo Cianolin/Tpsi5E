2d84e018da35d72f9dcf3440c1814d2bc2b5698b
Cianolin <145426146+Cianolin@users.noreply.github.com>
2024-10-01 08:28:35 +0200

    Add files via upload

---
+++ es3°.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+/*campi: ModelloAuto, marca, cilindrata, prezzo, anno_immatricolazione, determinare:
+1) Il prezzo + alto e quello + basso visualizzando anche il modello dell&#39;auto;
+2) Ordinare la lista delle auto in base all&#39;anno d&#39;immatricolazione;
+3) Visualizzare il nome delle auto a partire da un prezzo scelto dall&#39;utente.*/
+
+struct Auto{
+ string ModelloAuto;
+ string marca;
+ double prezzo;
+ int cilindrata;
+ time anno_inmadricolazione;
+}
+
+int main() {
+    return 0;
+}
