d5bc2d9fee7ad9a35a82889bf56aed5cb268eec9
Luciano Lin <luciano.lin@iisviolamarchesini.edu.it>
2024-10-02 18:27:28 +0200

    es3

--- es3°.c
+++ es3°.c
@@ -12,40 +12,53 @@ typedef struct
 {
     char modelloAuto[40];
     char marca[40];
-    double prezzo;
+    float prezzo;
     int cilindrata;
     int anno_inmadricolazione;
 } Auto;
-void Visualizza(struct Auto garage[])
+void Visualizza(Auto garage[], int i)
 {
+    printf("Prezzo auto %2f\n", garage[i].prezzo);
+    printf("Modello auto %s", garage[i].modelloAuto);
+}
+// 1
+int MaxPrezzo(Auto garage[])
+{
+    double maxPrezzo = 0;
+    int posizione;
     for (int i = 0; i < strlen(garage); i++)
     {
-        printf("Prezzo auto %2f\n", auto.prezzo);
-        printf("Modello auto %s", auto.modelloAuto);
-    }
-}
-//1
-void VisualizzaPerPrezzo(struct Auto garage[])
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
+        if (maxPrezzo < garage[i].prezzo)
         {
-            if (garage.prezzo[j] > garage.prezzo[j + 1])
+            maxPrezzo = garage[i].prezzo;
+            posizione = i;
+        }
+    }
+    return posizione;
+}
+int MinPrezzo(Auto garage[])
+{
+    double minPrezzo = 0;
+    int posizione = 0;
+    for (int i = 0; i < strlen(garage); i++)
+    {
+        double minPrezzo = garage[i].prezzo;
+        for (int i = 0; i < strlen(garage); i++)
+        {
+            if (minPrezzo > garage[i].prezzo)
             {
-                int temp = garage[j];
-                garage[j] = garage[j + 1];
-                garage[j + 1] = temp;
+                minPrezzo = garage[i].prezzo;
             }
         }
     }
+    return posizione;
 }
 // 2
-void OrdinaPerAnno(struct Auto arr[])
+void OrdinaPerAnno(Auto arr[])
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < 3 - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < 3 - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
@@ -57,37 +70,69 @@ void OrdinaPerAnno(struct Auto arr[])
     }
 }
 // 3
-void PrezzoAuto(struct Auto garage[], double sceltaPrezzo)
+void PrezzoAuto(Auto garage[], double sceltaPrezzo)
 {
-    printf("Da quel prezzo in poi ci sono:\n") for (int i = 0; i < strlen(garage); i++)
+    printf("Da quel prezzo in poi ci sono:\n");
+    for (int i = 0; i < strlen(garage); i++)
     {
         if (garage[i].prezzo >= sceltaPrezzo)
         {
-            printf("%c\n", garage[i].modelloAuto);
+            printf("%s\n", garage[i].modelloAuto);
         }
     }
 }
 int main()
 {
-    struct Auto garege[20];
-    Auto auto1;
+    Auto auto1 = {
+        .modelloAuto = "Fiat Panda",
+        .marca = "Fiat",
+        .prezzo = 15000.0,
+        .cilindrata = 1300,
+        .anno_inmadricolazione = 2015};
+
+    Auto auto2 = {
+        .modelloAuto = "Volkswagen Golf",
+        .marca = "Volkswagen",
+        .prezzo = 20000.0,
+        .cilindrata = 1600,
+        .anno_inmadricolazione = 2018};
+
+    Auto auto3 = {
+        .modelloAuto = "Toyota Yaris",
+        .marca = "Toyota",
+        .prezzo = 12000.0,
+        .cilindrata = 1000,
+        .anno_inmadricolazione = 2012};
+    Auto garege[3] = {auto1, auto2, auto3};
     int choose;
-    switch (choose)
+    do
     {
-    case 1:
-        Visualizza(garege);
-        break;
-    case 2:
-        OrdinaPerAnno(garege);
-        break;
-    case 3:
-        double prezzo;
-        printf("Scegli un prezzo da cui partire a cercare:\n");
-        scanf("%f\n", prezzo);
-        PrezzoAuto(garege, prezzo);
-        Visualizza(garege);
-    default:
-        break;
-    }
-    return 0;
+        printf("1. Visualizza l'auto più costosa e meno costosa\n2. Ordina le auto per anno di immatricolazione\n3. Visualizza le auto a partire dal prezzo che inserisci\n4. Visualizza tutte le auto");
+        scanf("%d", &choose);
+        switch (choose)
+        {
+        case 1:
+            printf("Auto con prezzo più alto\n");
+            Visualizza(garege, MaxPrezzo(garege));
+            printf("Auto con prezzo più basso\n");
+            Visualizza(garege, MinPrezzo(garege));
+            break;
+        case 2:
+            OrdinaPerAnno(garege);
+            break;
+        case 3:
+            float prezzo;
+            printf("Scegli un prezzo da cui partire a cercare:\n");
+            scanf("%2f\n", prezzo);
+            PrezzoAuto(garege, prezzo);
+        case 4:
+            for (int i = 0; i < strlen(garege); i++)
+            {
+                Visualizza(garege, i);
+            }
+        default:
+            break;
+        }
+        return 0;
+    } while (1);
 }
