fedf701e507193d6d4a50cb0c844e8b7dd773e6a
Cianolin <luciano.lin@iisviolamarchesini.edu.it>
2024-10-02 21:28:25 +0200

    es3 fine

--- es3°.c
+++ es3°.c
@@ -1,138 +1,141 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-/*campi: ModelloAuto, marca, cilindrata, prezzo, anno_immatricolazione, determinare:
-1) Il prezzo + alto e quello + basso visualizzando anche il modello dell&#39;auto;
-2) Ordinare la lista delle auto in base all&#39;anno d&#39;immatricolazione;
-3) Visualizzare il nome delle auto a partire da un prezzo scelto dall&#39;utente.*/
 
-typedef struct
-{
+typedef struct {
     char modelloAuto[40];
     char marca[40];
     float prezzo;
     int cilindrata;
     int anno_inmadricolazione;
 } Auto;
-void Visualizza(Auto garage[], int i)
-{
-    printf("Prezzo auto %2f\n", garage[i].prezzo);
-    printf("Modello auto %s", garage[i].modelloAuto);
+
+void Visualizza(Auto garage[], int i) {
+    printf("Prezzo auto: %.2f\n", garage[i].prezzo);
+    printf("Modello auto: %s\n", garage[i].modelloAuto);
+    printf("Anno di immatricolazione: %d\n", garage[i].anno_inmadricolazione);
 }
+
 // 1
-int MaxPrezzo(Auto garage[])
-{
-    double maxPrezzo = 0;
-    int posizione;
-    for (int i = 0; i < strlen(garage); i++)
-    {
-        if (maxPrezzo < garage[i].prezzo)
-        {
+int MaxPrezzo(Auto garage[], int size) {
+    float maxPrezzo = garage[0].prezzo;
+    int posizione = 0;
+    for (int i = 1; i < size; i++) {
+        if (maxPrezzo < garage[i].prezzo) {
             maxPrezzo = garage[i].prezzo;
             posizione = i;
         }
     }
     return posizione;
 }
-int MinPrezzo(Auto garage[])
-{
-    double minPrezzo = 0;
+
+int MinPrezzo(Auto garage[], int size) {
+    float minPrezzo = garage[0].prezzo;
     int posizione = 0;
-    for (int i = 0; i < strlen(garage); i++)
-    {
-        double minPrezzo = garage[i].prezzo;
-        for (int i = 0; i < strlen(garage); i++)
-        {
-            if (minPrezzo > garage[i].prezzo)
-            {
-                minPrezzo = garage[i].prezzo;
-            }
+    for (int i = 1; i < size; i++) {
+        if (minPrezzo > garage[i].prezzo) {
+            minPrezzo = garage[i].prezzo;
+            posizione = i;
         }
     }
     return posizione;
 }
+
 // 2
-void OrdinaPerAnno(Auto arr[])
-{
-    for (int i = 0; i < 3 - 1; i++)
-    {
-        for (int j = 0; j < 3 - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
+void OrdinaPerAnno(Auto arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j].anno_inmadricolazione > arr[j + 1].anno_inmadricolazione) {
+                Auto temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
 }
+
 // 3
-void PrezzoAuto(Auto garage[], double sceltaPrezzo)
-{
+void PrezzoAuto(Auto garage[], int size, float sceltaPrezzo) {
     printf("Da quel prezzo in poi ci sono:\n");
-    for (int i = 0; i < strlen(garage); i++)
-    {
-        if (garage[i].prezzo >= sceltaPrezzo)
-        {
+    for (int i = 0; i < size; i++) {
+        if (garage[i].prezzo >= sceltaPrezzo) {
             printf("%s\n", garage[i].modelloAuto);
         }
     }
 }
-int main()
-{
+
+int main() {
     Auto auto1 = {
         .modelloAuto = "Fiat Panda",
         .marca = "Fiat",
         .prezzo = 15000.0,
         .cilindrata = 1300,
-        .anno_inmadricolazione = 2015};
+        .anno_inmadricolazione = 2015
+    };
 
     Auto auto2 = {
         .modelloAuto = "Volkswagen Golf",
         .marca = "Volkswagen",
         .prezzo = 20000.0,
         .cilindrata = 1600,
-        .anno_inmadricolazione = 2018};
+        .anno_inmadricolazione = 2018
+    };
 
     Auto auto3 = {
         .modelloAuto = "Toyota Yaris",
         .marca = "Toyota",
         .prezzo = 12000.0,
         .cilindrata = 1000,
-        .anno_inmadricolazione = 2012};
-    Auto garege[3] = {auto1, auto2, auto3};
+        .anno_inmadricolazione = 2012
+    };
+
+    Auto garage[3] = {auto1, auto2, auto3};
+    int size = sizeof(garage) / sizeof(garage[0]);
     int choose;
-    do
-    {
-        printf("1. Visualizza l'auto più costosa e meno costosa\n2. Ordina le auto per anno di immatricolazione\n3. Visualizza le auto a partire dal prezzo che inserisci\n4. Visualizza tutte le auto");
+
+    do {
+        printf("1. Visualizza l'auto più costosa e meno costosa\n");
+        printf("2. Ordina le auto per anno di immatricolazione\n");
+        printf("3. Visualizza le auto a partire dal prezzo che inserisci\n");
+        printf("4. Visualizza tutte le auto\n");
+        printf("5. Esci\n");
         scanf("%d", &choose);
-        switch (choose)
-        {
-        case 1:
-            printf("Auto con prezzo più alto\n");
-            Visualizza(garege, MaxPrezzo(garege));
-            printf("Auto con prezzo più basso\n");
-            Visualizza(garege, MinPrezzo(garege));
-            break;
-        case 2:
-            OrdinaPerAnno(garege);
-            break;
-        case 3:
-            float prezzo;
-            printf("Scegli un prezzo da cui partire a cercare:\n");
-            scanf("%2f\n", prezzo);
-            PrezzoAuto(garege, prezzo);
-        case 4:
-            for (int i = 0; i < strlen(garege); i++)
-            {
-                Visualizza(garege, i);
-            }
-        default:
-            break;
+
+        switch (choose) {
+            case 1:
+                printf("Auto con prezzo più alto\n");
+                Visualizza(garage, MaxPrezzo(garage, size));
+                printf("Auto con prezzo più basso\n");
+                Visualizza(garage, MinPrezzo(garage, size));
+                break;
+            case 2:
+                OrdinaPerAnno(garage, size);
+                printf("Auto ordinate per anno di immatricolazione:\n");
+                for (int i = 0; i < size; i++) {
+                    Visualizza(garage, i);
+                }
+                break;
+            case 3:
+                {
+                    float prezzo;
+                    printf("Scegli un prezzo da cui partire a cercare:\n");
+                    scanf("%f", &prezzo);
+                    PrezzoAuto(garage, size, prezzo);
+                }
+                break;
+            case 4:
+                for (int i = 0; i < size; i++) {
+                    Visualizza(garage, i);
+                }
+                break;
+            case 5:
+                return 0;
+            default:
+                printf("Scelta non valida. Riprova.\n");
+                break;
         }
-        return 0;
     } while (1);
+
+    return 0;
 }
