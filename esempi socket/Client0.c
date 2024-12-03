2c6767c79de041eabc6eba48cf7254beaaecc020
Luciano Lin <luciano.lin@iisiolamarchesini.edu.it>
2024-12-03 09:17:41 +0100

    continuo pagina web

--- esempi socket/Client0.c
+++ esempi socket/Client0.c
@@ -1,19 +1,20 @@
 // CLIENT
-#include <stdio.h>      //std in-out
-#include <stdlib.h>     //per utilizzo di certe funzioni:htonl,rand,....
-#include <sys/socket.h> //funz. accept+bind+listen
-#include <sys/types.h>  //funz. accept
-#include <netinet/in.h> //definiscono la struttura degli indirizzi
-#include <string.h>     //funz. stringhe
-#include <errno.h>      //gestioni errori connessione
-#include <ctype.h>      //bind
-#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <sys/socket.h> 
+#include <sys/types.h>  
+#include <netinet/in.h> 
+#include <string.h>  
+#include <errno.h>   
+#include <ctype.h>      
+#include <unistd.h>  
 
 #define DIM 50
 #define SERVERPORT 1313
 
-int main(int argc, char **argv) struct sockaddr_in servizio;
+int main(int argc, char **argv) 
 {
+    struct sockaddr_in servizio;
     servizio.sin_family = AF_INET;
     servizio.sin_addr.s_addr = htonl(INADDR_ANY);
     servizio.sin_port = htons(SERVERPORT);
